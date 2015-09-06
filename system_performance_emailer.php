73bc3e84ae3c8b850d08851cdc20d67e94d1db20
catscarlet <i@catscarlet.com>
2015-09-06 16:49:43 +0800

    add emailer function

---
+++ system_performance_emailer.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'json-format/format_json.php';
+require_once 'smtp/sendmail.php';
+$filepath = 'monitor.json';
+$readcpustathistory = 1;
+exec("tail -n $readcpustathistory $filepath", $system_performance_monitor);
+$readhistorymax = min(count($system_performance_monitor), $readcpustathistory);
+$i = $readhistorymax;
+foreach ($system_performance_monitor as $key => $value) {
+    --$i;
+    $monitor[$i] = json_decode($value, true);
+}
+$system_performance_info = format_json($value);
+//$system_performance_info="this is a text";
+$time = $monitor[0]['TIME'];
+sendemailbysmtp('Your system_performance_info at '.date('F j, Y, g:i a', $time), $system_performance_info);
+echo $system_performance_info;
